26b6a3fad5ece86cd6093e3ceba24bfec68b131f
Laura Luca <Laura.Luca@endava.com>
2017-08-16 12:45:01 +0300

    heroku java fix

---
+++ src/main/java/com/travel/blog/NomapsApplication.java
@@ -0,0 +1,12 @@
+package com.travel.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomapsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/travel/blog/NomapsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.travel.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NomapsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
